d5dffc7a143a93f7eb9a860fd259ab4580cc9079
lango1989 <yaowenlang1989@163.com>
2016-10-21 15:54:59 +0800

    modify binary

--- binary.c
+++ binary.c
@@ -1,42 +1,47 @@
 #include<stdio.h>
-void binary(int num);
+void binary_loop(long num);
+void binary(long num);
 
 int main(void)
 {
-	int num=0;
-	int i=0;
-	int temp[20]={5};
-	int a=0;
-	puts("Enter a number; ");
-	scanf("%d", &num);
-	a=num;
+	long num=0;
+	puts("请输入一个正整数: ");
+	scanf("%ld", &num);
 	if (num==0)
-		printf("%d", num);
-	else
-	{
+		printf("%ld", num);
+	printf("使用递归：");
+	binary(num);
+	printf("\n");
+	printf("使用循环：");
+	binary_loop(num);
+	printf("\n");
+
+	return 0;
+}
+
+
+//使用循环
+void binary_loop(long num)
+{
+	int i=0;
+	long temp[100];
 		for(i=0; num>0; i++, num=num/2)
 		{
 			temp[i]=num%2;
 		}
 		for(int j=i-1; j>=0; j--)
 		{
-			printf("%d", temp[j]);
+			printf("%ld", temp[j]);
 
 		}
-	}
-	printf("\n");
-	binary(a);
-	printf("\n");
-
-	return 0;
 }
-
-void binary(int num)
+// 用递归
+void binary(long num)
 {
 	if(num>0)
 	{
 		binary(num/2);
-		printf("%d", num%2);
+		printf("%ld", num%2);
 	}
 }
 
